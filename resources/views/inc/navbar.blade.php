fb09adf9f895ee1eea3eb89a08fe92573f5987e8
jenishls <jlshrestha90@gmail.com>
2018-02-01 15:28:12 +0545

    navbar href

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,43 +1,13 @@
 
-{{--  
-<nav class="navbar  navbar-expand-xs  justify-content-between" >
-    <a href="/" class="navbar-brand " style="color: white;">
-      <h2>SURPRISE ME</h2 >
-    </a>
-   
-    
-    
-  
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <form class="form-inline">
-                <div class="input-group">
-                  
-                  <input type="text" class="form-control" placeholder="Search ...." >
-                  <span class="input-group-btn">
-                    <a class="btn btn-md btn-surprise navbar-text" role="button"><i class="fa fa-search"></i></a>
-                  </span>
-                  
-                  
-                  <a href="#" class="btn btn-md btn-surprise navbar-text" role="button"><i class="fa fa-shopping-cart"></i></a>    
-         
-                  <a href="#" class="btn btn-md btn-surprise navbar-text" role="button"><i class="fa fa-user"></i></a>
-                </div>
-              </form>
-            </li>
-              
-       
-          
-        </ul>
- 
-    
-
- </nav>  --}}
 <div class="container-fluid navv py-2">
 <div class="row py-1 ">
   
   <div class="col-sm-2 ">
-    <h2 class="navbar-brand">Surprise Me</h2>
+    <a href="/" class="links">
+      <h2 class="navbar-brand">
+        Surprise Me
+      </h2>
+    </a>
   </div>
   
   <div class="col-sm-7">
@@ -73,10 +43,22 @@
   <div class="container-fluid btn-surprise py-1" >
     <div class="row text-center">
       <div class="col-1"></div>
-      <div class="col-2">Men</div>
-      <div class="col-3">Women</div>
-      <div class="col-2">Kids</div>
-      <div class="col-2">Other</div>
+      <div class="col-2">
+        <a href="/category/1" class="links">Men</a>
+      </div>
+      
+      <div class="col-3">
+        <a href="/category/2" class="links">Women</a>
+      </div>
+      
+      <div class="col-2">
+        <a href="/category/3" class="links">Kids</a>
+      </div>
+      
+      <div class="col-2">
+        <a href="/category/4" class="links">Other</a>
+      </div>
+      
       <div class="col-1"></div>
     </div>
     
